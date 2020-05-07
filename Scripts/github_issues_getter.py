fedc79834d5e91c51bfd7c141cabb11843da9075
MateusSantosFonseca <mateus.santos.fsc@gmail.com>
2020-05-07 10:48:05 -0300

    Adicionado script exportador para csv, adicionado repositórios analisados

--- Scripts/github_issues_getter.py
+++ Scripts/github_issues_getter.py
@@ -1,15 +1,25 @@
 import requests
 
-def requisitar_issues(headers, query):
-    request = requests.post('https://api.github.com/graphql', json = {'query': query}, headers = headers)
-    if request.status_code == 200:
-        return request.json()
-    elif request.status_code == 502:
-        return requisitar_issues(query, headers)
-    else:
-        raise Exception("A query falhou: {}. {}".format(request.status_code, query))
+def requisitar_issues(headers, query, nome_repositorio, path_repositorios_analisados):
+    try:
+        request = requests.post('https://api.github.com/graphql', json = {'query': query}, headers = headers)
+        if request.status_code == 200:
+            return request.json()
+        elif request.status_code == 502:
+            return requisitar_issues(query, headers, nome_repositorio, path_repositorios_analisados)
+        else:
+            raise Exception("A query falhou: {}. {}".format(request.status_code, query))
+    except:
+        erro = f"Ocorreu um erro com a API do GitHub e as issues do repositório {nome_repositorio} não puderam ser obtidas.\n"
+        escrever_log_erro(erro, path_repositorios_analisados)
+        print(erro)
+            
+def escrever_log_erro(erro, path_repositorios_analisados):
+    path_arquivo_erro = path_repositorios_analisados + "\\log_erros.txt"
+    with open(path_arquivo_erro, "a+") as myfile:
+        myfile.write(erro)         
 
-def get_issues(headers, query_issues, orientacao_ordenacao, repositorio):
+def get_issues(headers, query_issues, orientacao_ordenacao, repositorio, path_repositorios_analisados):
     nome_owner_repositorio = repositorio['nameWithOwner'].split('/')
     owner_repositorio = nome_owner_repositorio[0]
     nome_repositorio = nome_owner_repositorio[1]
@@ -18,8 +28,11 @@ def get_issues(headers, query_issues, orientacao_ordenacao, repositorio):
     query_issues_final = query_issues_final.replace("{placeholder_owner_repo}", owner_repositorio)
     query_issues_final = query_issues_final.replace("{placeholder_orientacao_issue}", orientacao_ordenacao)
     
-    response = requisitar_issues(headers, query_issues_final)
-    issues_retornadas = response["data"]["repository"]["issues"]["nodes"]
-
-    print(f"\nA recuperação das issues do repositório {nome_repositorio} foi finalizada.") 
+    response = requisitar_issues(headers, query_issues_final, nome_repositorio, path_repositorios_analisados)
+    
+    issues_retornadas = []
+    
+    if(response is not None):
+        issues_retornadas = response["data"]["repository"]["issues"]["nodes"]
+    
     return issues_retornadas
--- main.py
+++ main.py
@@ -3,27 +3,42 @@ from Util.query_repos import query_repositorios
 from Util.query_issues import query_issues
 from Scripts.github_repo_getter import get_repositorios
 from Scripts.github_issues_getter import get_issues
+from Scripts.exportador_csv import exportar_arquivos_csv
 import pathlib
 import os
+import shutil
 
 def criar_pasta(path):
     try:
         os.mkdir(path)
     except FileExistsError:
-        print("\nDiretório:", path, "não foi criado pois já existe.")
+        print(f"\nDiretório: {path} não foi criado pois já existe.")
+        
+def deletar_pasta(path):
+    try:
+        shutil.rmtree(path)
+    except:
+        print(f"\nDiretório: {path} não pode ser excluido. Favor apagá-lo manualmente.")
 
 def main_script():
     repositorios = get_repositorios(headers, query_repositorios)
     
     path_repositorios_analisados = str(pathlib.Path().absolute()) + "\\RepositoriosAnalisados"
+    
+    deletar_pasta(path_repositorios_analisados)
     criar_pasta(path_repositorios_analisados)
     
     for i, repositorio in enumerate(repositorios):
-        top_50_issues = get_issues(headers, query_issues, "DESC" , repositorio)
-        bottom_50_issues = get_issues(headers, query_issues, "ASC", repositorio)
-        
-        # Cria uma pasta dentro de /RepositoriosAnalisados com o nome do repositorio e um indice atras
-        # Pega as issues desse repositorio com a query
-        # Exporta pra .csv tanto o repositorio quanto a issue
-        
+        top_issues = get_issues(headers, query_issues, "DESC" , repositorio, path_repositorios_analisados)
+        bottom_issues = get_issues(headers, query_issues, "ASC", repositorio, path_repositorios_analisados)
+            
+        path_repo_analisado = path_repositorios_analisados + "\\" + "{:04n}".format(i) + "_" + repositorio['name']
+        criar_pasta(path_repo_analisado)
+            
+        exportar_arquivos_csv(path_repo_analisado, repositorio, top_issues, bottom_issues)
+            
+        print(f"\nO repositório {repositorio['name']} foi analisado e os resultados retornados foram exportados para um arquivo .csv com sucesso.") 
+            
+    print("\nO Script foi finalizado com sucesso.")   
+
 main_script()
